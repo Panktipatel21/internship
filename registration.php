5e36ae5d150623f38c26da299d2a7ca3050f76f1
pankti patel <84764279+Panktipatel21@users.noreply.github.com>
2021-05-27 11:19:25 +0530

    day 1
    student registration

---
+++ registration.php
@@ -0,0 +1,27 @@
+<table width="60%"  border="2" align="center">
+
+<th colspan=2> registration </th>
+<tr><td> first name </td> <td> <input type ="text" name="fnmae" /> </td> </tr>
+<tr><td> last name  </td><td> <input type="text" name="lname" />  </td></tr>
+<tr><td> address  </td><td> <textarea name = "address">  </textarea>  </td></tr>
+<tr><td> city </td> <td> <input type ="text" name="city" /> </td> </tr>
+<tr><td> state </td> <td> <select name= "state">
+<option>gujrat</option>
+<option>up</option>
+<option>mp</option>
+<option>zarkhand</option>
+</select> </td> </tr>
+
+<tr>
+<td> gender </td> 
+<td> <input type="radio"  name="gender"   value="male" />male
+  <input type="radio"  name="gender"   value="female" />female
+</td></tr>
+<tr><td>  Photo  </td><td> <input type="file" name="photo" />  </td></tr>
+<tr><td>  Hobby  </td><td> 
+
+<input type="checkbox" name="hobby" value="dance" /> dance
+<input type="checkbox" name="hobby" value="reading" /> reading
+<input type="checkbox" name="hobby" value="movie" /> movie </td></tr>
+
+</table>
